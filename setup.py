247ef68872b1326de0996d0efd540927af4e594b
Dimitry Fourés <dimitry.foures@rkrepsilon.com>
2017-06-13 14:17:46 +0100

    setup.py readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='openoutcry',
     version='0.1.0',
-    description="Open Outcry is an open source project for the development of reinforcement learning algorithms in the context of trading.",
+    description="Open Outcry is an open-source project for the development of reinforcement learning algorithms in the context of trading.",
     author="Prediction Machines",
     author_email='openoutcry@prediction-machines.com',
     url='https://github.com/prediction-machines/openoutcry',
